66305e868a9adba4e38ff2195ce5a2384c50d429
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2022-02-15 13:18:53 +0100

    GUI implementation

---
+++ src/GUItictactoe.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GUItictactoe {
+    private JButton tttbutton1;
+    private JButton tttbutton3;
+    private JButton tttbutton2;
+    private JButton tttbutton7;
+    private JButton tttbutton8;
+    private JButton tttbutton9;
+    private JButton tttbutton6;
+    private JButton tttbutton5;
+    private JButton tttbutton4;
+}
