eb224273f959f84e2b9c3153235c65ba9d25978a
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2022-02-21 11:10:56 +0100

    Beginning of Java

--- src/GUItictactoe.java
+++ src/GUItictactoe.java
@@ -2,12 +2,21 @@ import javax.swing.*;
 
 public class GUItictactoe {
     private JButton tttbutton1;
-    private JButton tttbutton3;
+
     private JButton tttbutton2;
-    private JButton tttbutton7;
-    private JButton tttbutton8;
-    private JButton tttbutton9;
-    private JButton tttbutton6;
-    private JButton tttbutton5;
+
+    private JButton tttbutton3;
+
     private JButton tttbutton4;
+
+    private JButton tttbutton5;
+
+    private JButton tttbutton6;
+
+    private JButton tttbutton7;
+
+    private JButton tttbutton8;
+
+    private JButton tttbutton9;
+
 }
--- src/tictactoe.java
+++ src/tictactoe.java
@@ -1,5 +1,25 @@
 import javax.swing.*;
 
 public class tictactoe {
-    String s = JOptionPane.showInputDialog("yo");
+    public static void main(String[] args) {
+
+        String one = " ";
+        String two = " ";
+        String three = " ";
+        String four = " ";
+        String five = " ";
+        String six = " ";
+        String seven = " ";
+        String eight = " ";
+        String nine = " ";
+
+        String board = "| 1 | 2 | 3 |\n| 4 | 5 | 6 |\n| 7 | 8 | 9 |";
+        char[] boardC = board.toCharArray();
+
+        while() {
+            String pick = JOptionPane.showInputDialog(board + "\nWhich spot will you pick?");
+            int pickI = Integer.parseInt(pick);
+        }
+
+    }
 }
